b68bc2977966e655677e0cb258bb213e5835a693
prcela <kresimir.prcela@gmail.com>
2016-01-18 11:39:46 +0100

    Update Config.swift

--- SwiftyPDF/Config.swift
+++ SwiftyPDF/Config.swift
@@ -10,8 +10,15 @@ import UIKit
 
 struct Config
 {
+    // defines the actual content size of scroll view. It is equal to real pdf size * pdf size magnifier
     static let contentSizeMagnifier:CGFloat = 2
+    
+    // extra zoom allows to go beyond content size
     static let extraZoom: CGFloat = 1
+    
+    // size of tile 
     static let tileSize = CGSize(width: 256, height: 256)
+    
+    // show tiles grid
     static let showTileLines = true
 }
